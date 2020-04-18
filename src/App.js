90b3ac236a619a4d6dfd6b54a12def58aab2ed60
Michal Oktabec <info@michaloktabec.cz>
2020-04-18 12:53:10 +0200

    Add not found route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './Login';
 import Home from './Home';
 import Signup from './Signup';
@@ -13,9 +13,12 @@ const App = () => {
     <AuthProvider>
       <BrowserRouter>
         <div>
-          <PrivateRoute exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={Signup} />
+          <Switch>
+            <PrivateRoute exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route path="*" component={() => 'NOT FOUND'} />
+          </Switch>
         </div>
       </BrowserRouter>
     </AuthProvider>
